e037f31b461fe4dff6c559bf1a64835289397de9
Nils GB <nilsgrossebeikel@gmail.com>
2026-05-22 11:58:39 +0200

    add notifier

--- src/app/AppUI.tsx
+++ src/app/AppUI.tsx
@@ -1,10 +1,12 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
+import { Notifier } from "@open-pioneer/notifier";
 import { MapComponent } from "./components/MapComponent";
 
 export function AppUI() {
     return (
         <div style={{ display: "flex", flexDirection: "column", height: "100vh", width: "100%" }}>
+            <Notifier />
             <MapComponent></MapComponent>
         </div>
     );
--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Notifier } from "@open-pioneer/notifier";
 import { DefaultMapProvider, MapAnchor, MapContainer, useMapModel } from "@open-pioneer/map";
 
 const MAP_ID = "main";
@@ -14,7 +13,6 @@ export function MapComponent() {
 
     return (
         <div style={{ width: "100%", height: "100%" }}>
-            <Notifier />
             <DefaultMapProvider map={map}>
                 <MapContainer aria-label="webgis map">
                     <MapAnchor />
